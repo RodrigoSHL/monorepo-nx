5d1420e50b78cce9865feb7c97a15531e7c8f899
RodrigoSHL <shl.catalan@gmail.com>
2025-02-20 16:31:50 -0300

    chore: testing in pipeline
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- apps/user-api/src/app/app.service.ts
+++ apps/user-api/src/app/app.service.ts
@@ -5,8 +5,4 @@ export class AppService {
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
-
-  getDato(): { message: string } {
-    return { message: 'Hello API dato' };
-  }
 }
